d1e5febc8fdd18508d424539cbb30d28a28ed829
ShockHit <mr.edenhazani@gmail.com>
2020-04-02 18:46:36 +0300

    Updates

---
+++ precache-manifest.f8b7c36297d9cc4e9db6e86f809299c5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf847d5910e445b9180ee41b99837ac4",
+    "url": "/Morbiez/index.html"
+  },
+  {
+    "revision": "cc3e7ae99d4ac512250d",
+    "url": "/Morbiez/static/css/2.20270b56.chunk.css"
+  },
+  {
+    "revision": "453c5103bf64e01c5aea",
+    "url": "/Morbiez/static/css/main.e54cc82d.chunk.css"
+  },
+  {
+    "revision": "cc3e7ae99d4ac512250d",
+    "url": "/Morbiez/static/js/2.12d17ab6.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Morbiez/static/js/2.12d17ab6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "453c5103bf64e01c5aea",
+    "url": "/Morbiez/static/js/main.ac90c4ee.chunk.js"
+  },
+  {
+    "revision": "9f50657dc5ef24b96619",
+    "url": "/Morbiez/static/js/runtime-main.c6eba1cc.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Morbiez/precache-manifest.f8b7c36297d9cc4e9db6e86f809299c5.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Morbiez/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
