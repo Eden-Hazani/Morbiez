d7a01f250e05b1e474bafbc529783f1a2cf66519
ShockHit <mr.edenhazani@gmail.com>
2020-04-19 16:02:29 +0300

    Updates

---
+++ precache-manifest.69a3a441ee02270c54b84d29be7752b7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a54f18546d3b42fe51fddea47994e409",
+    "url": "/Morbiez/index.html"
+  },
+  {
+    "revision": "d003eef5f8e365ade87f",
+    "url": "/Morbiez/static/css/2.20270b56.chunk.css"
+  },
+  {
+    "revision": "f7dc3ec90ef6e7917c39",
+    "url": "/Morbiez/static/css/main.121658ac.chunk.css"
+  },
+  {
+    "revision": "d003eef5f8e365ade87f",
+    "url": "/Morbiez/static/js/2.ffc4e08d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Morbiez/static/js/2.ffc4e08d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7dc3ec90ef6e7917c39",
+    "url": "/Morbiez/static/js/main.92722d83.chunk.js"
+  },
+  {
+    "revision": "9f50657dc5ef24b96619",
+    "url": "/Morbiez/static/js/runtime-main.c6eba1cc.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Morbiez/precache-manifest.4f120de9100562f23c906fafe5d23f3e.js"
+  "/Morbiez/precache-manifest.69a3a441ee02270c54b84d29be7752b7.js"
 );
 
 self.addEventListener('message', (event) => {
